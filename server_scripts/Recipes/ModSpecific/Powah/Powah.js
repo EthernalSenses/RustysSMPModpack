8080708efed61da65fae9b2882e23a971e1a020a
Rusty <awercode05@gmail.com>
2022-07-30 13:38:21 +0300

    s

--- server_scripts/Recipes/ModSpecific/Powah/Powah.js
+++
@@ -1,3 +0,0 @@
-onEvent('recipes', e => {
-            energize(e, ['#forge:ingots/steel', '#forge:ingots/gold', '#forge:dusts/redstone', '#forge:dusts/glowstone'], 'powah:steel_energize\', 20000, 1)
-            })
